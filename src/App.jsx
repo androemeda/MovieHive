156dd18d6525c50eb9e9dee551d6c2ff328a1c96
androemeda <kartikdeshpande2810@gmail.com>
2024-12-07 02:17:26 +0530

    implemented context-api and toggle emoji feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,34 @@ import Banner from './components/Banner'
 import Movies from './components/Movies'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import FavoriatesPage from './components/FavoriatesPage'
+import React, { useState , createContext } from 'react'
+
+export const AppContext = React.createContext();
 
 function App() {
+
+  let [favs , setFavs] = useState([]);
+
   return (
-    <BrowserRouter>
+    <AppContext.Provider value={{favs , setFavs}}>
+      <BrowserRouter>
       <NavBar />
-      <div className='mt-[80px]'> {/* Adjust this margin to match NavBar height */}
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                <Banner />
-                <Movies />
-              </>
-            }
-          ></Route>
-          <Route path='/fav' element={<FavoriatesPage />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <div className='mt-[80px]'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <Banner />
+                  <Movies />
+                </>
+              }
+            ></Route>
+            <Route path='/fav' element={<FavoriatesPage />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import { AppContext } from '../App';
 
 function Movies() {
 
@@ -7,12 +8,25 @@ function Movies() {
 
   let [hoverId , setHoverId] = useState(null);
 
+  let {favs , setFavs} = useContext(AppContext);
+
   const showEmoji = (id) => {
     setHoverId(id);
   }
 
   const hideEmoji = () => {
     setHoverId(null);
+  }
+
+  const ToggleEmoji = (id) => {
+    if(!favs.includes(id)){
+      let arr = [...favs , id];
+      setFavs(arr);
+    }
+    else{
+      let arr = favs.filter((fav) => {return fav !== id});
+      setFavs(arr);
+    }
   }
 
   useEffect(() => {
@@ -42,7 +56,8 @@ function Movies() {
             <div className='rounded-lg absolute flex bottom-0 bg-gray-400 w-full opacity-70 justify-center items-center h-[10%]'>{movie.title || movie.name}</div>
             <button className='absolute aspect-square h-[10%] bg-black top-2 right-3 rounded-lg justify-center items-center text-xl'
               style={{display : (movie.id == hoverId) ? "flex" : "none"}}
-            >😍</button>
+              onClick={() => ToggleEmoji(movie.id)}
+            >{(favs.includes(movie.id) ? "❌" : "😍")}</button>
           </div>
         )
       })}
