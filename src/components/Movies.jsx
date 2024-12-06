da9f62a9235dd7ca1b3aa53233a00d34289ef13c
androemeda <kartikdeshpande2810@gmail.com>
2024-12-07 01:45:44 +0530

    emojis added

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,6 +5,16 @@ function Movies() {
 
   let [movies , setMovies] = useState([]);
 
+  let [hoverId , setHoverId] = useState(null);
+
+  const showEmoji = (id) => {
+    setHoverId(id);
+  }
+
+  const hideEmoji = () => {
+    setHoverId(null);
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -22,11 +32,19 @@ function Movies() {
 
   return (
     <div className='flex flex-wrap justify-center items-center'>
-      {movies.map((movie) => {
-        return <div key={movie.id} className='m-2 w-1/5 relative hover:scale-110 transition transform duration-300 rounded-lg'>
-          <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path || movie.backdrop_path}`} className='rounded-lg'></img>
-          <div className='rounded-lg absolute flex bottom-0 bg-gray-400 w-full opacity-70 justify-center items-center h-[10%]'>{movie.title || movie.name}</div>
-        </div>
+      {(movies.length == 0) ? <div>...Loading</div> : movies.map((movie) => {
+        return (
+          <div key={movie.id} className='m-2 w-1/5 relative hover:scale-110 transition transform duration-300 rounded-lg'
+            onMouseEnter={() => showEmoji(movie.id)}
+            onMouseLeave={hideEmoji}
+          >
+            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path || movie.backdrop_path}`} className='rounded-lg'></img>
+            <div className='rounded-lg absolute flex bottom-0 bg-gray-400 w-full opacity-70 justify-center items-center h-[10%]'>{movie.title || movie.name}</div>
+            <button className='absolute aspect-square h-[10%] bg-black top-2 right-3 rounded-lg justify-center items-center text-xl'
+              style={{display : (movie.id == hoverId) ? "flex" : "none"}}
+            >😍</button>
+          </div>
+        )
       })}
     </div>
   );
